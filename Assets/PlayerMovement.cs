22e90290b4444d1c122b679e5e67c782d67479b9
Andrew Alsing <andrew32alsing@gmail.com>
2023-03-13 15:11:38 -0700

    added ability to jump and adjusted gravity settings

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
-
     public float runSpeed = 40f;
-
     float horizontalMove = 0f;
+    bool jump = false;
 
     // Update is called once per frame
     void Update()
     {
         // get user input (a, d, left arrow, right arrow)
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
     }
 
     void FixedUpdate() {
         // move character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+        Debug.Log(jump);
     }
 }
