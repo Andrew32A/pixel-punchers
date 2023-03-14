e198451a3cd5b350bc27bf4d26287fc4bc5ad525
Andrew Alsing <andrew32alsing@gmail.com>
2023-03-14 12:04:07 -0700

    added fist and tied collision detection to it

---
+++ Assets/FistMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FistMovement : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("fist moved");
+            Attack();
+        }
+    }
+
+    void Attack() {
+        transform.position = new Vector3(transform.position.x + 30, transform.position.y, transform.position.z);
+    }
+
+    // void FixedUpdate() {
+    //     transform.position = new Vector3(transform.position.x + 3, transform.position.y, transform.position.z);
+    // }
+}
