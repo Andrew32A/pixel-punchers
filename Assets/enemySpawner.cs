eacf6ee75ed2e1e89be643dffd5b7b3e76bc94ff
Andrew Alsing <andrew32alsing@gmail.com>
2023-03-16 20:36:23 -0700

    fixed spawn positioning and added ability to spawn on both sides

--- Assets/enemySpawner.cs
+++ Assets/enemySpawner.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class enemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public Camera cam;
     public float spawnInterval;
-    public float xPos = 100f;
+    public float xPos = 1200f;
     public float yPos = 100f;
 
     // Start is called before the first frame update
@@ -17,7 +18,8 @@ public class enemySpawner : MonoBehaviour
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy) {
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(xPos, yPos, 0), Quaternion.identity);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(cam.transform.position.x + xPos, cam.transform.position.y + yPos, 0), Quaternion.identity);
+        GameObject newEnemy2 = Instantiate(enemy, new Vector3(cam.transform.position.x + -xPos, cam.transform.position.y + yPos, 0), Quaternion.identity);
         StartCoroutine(spawnEnemy(interval, enemy));
     }
 }
