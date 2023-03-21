fb2d5dde07dd79bb5b270e0a2a35df3c1b90855b
Andrew Alsing <andrew32alsing@gmail.com>
2023-03-21 11:55:11 -0700

    minor updates

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
     private float length, startpos;
-    public GameObject camera;
+    public GameObject cam;
     public float parallaxEffect;
 
     void Start()
@@ -16,8 +16,8 @@ public class Parallax : MonoBehaviour
 
     void Update()
     {
-        float temp = (camera.transform.position.x * (1 - parallaxEffect));        
-        float dist = (camera.transform.position.x * parallaxEffect);
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));        
+        float dist = (cam.transform.position.x * parallaxEffect);
 
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
